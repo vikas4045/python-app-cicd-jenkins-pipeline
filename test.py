3a87b8e8cd15d13bc11c6c2340b2ae26429e4425
VikasAsawale <vikasasawale25@gmail.com>
2026-02-03 13:13:53 +0530

    Remove xmlrunner from test.py, use plain pytest/unittest

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import unittest
-import xmlrunner
 import app
 import re
 
@@ -10,38 +9,31 @@ class TestHello(unittest.TestCase):
         self.app = app.app.test_client()
 
     def test_hello(self):
-
         rv = self.app.get('/')
         self.assertEqual(rv.status, '200 OK')
 
         # searching for <h1> tag in the application data using regex
-
         message = rv.data.decode()
-        start = re.search("<h1>",message).start()
-        end = re.search("</h1>",message).start()
+        start = re.search("<h1>", message).start()
+        end = re.search("</h1>", message).start()
         message = message[start+4:end]
 
         self.assertEqual(message, "hello brother, Lets have some fun!!")
 
     def test_name(self):
-
         name = 'alice'
         rv = self.app.get(f'/{name}')
         self.assertEqual(rv.status, '200 OK')
 
         # searching for <h1> tag in the application data using regex
-
         message = rv.data.decode()
-        start = re.search("<h1>",message).start()
-        end = re.search("</h1>",message).start()
+        start = re.search("<h1>", message).start()
+        end = re.search("</h1>", message).start()
         message = message[start+4:end]
 
         self.assertEqual(message, "alice: A nerd")
 
-if __name__ == '__main__':
 
-    ############# To Generate Test Reports  #############
-    runner = xmlrunner.XMLTestRunner(output='test-reports')
-    unittest.main(testRunner=runner)
-    #####################################################
+if __name__ == '__main__':
+    # Only one main call, no xmlrunner
     unittest.main()
